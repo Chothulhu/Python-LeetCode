c08a6650b19f6f295467aed887306e9003f365cf
Veljko Vračarević <vracarevicveljko@gmail.com>
2024-07-31 22:58:22 +0200

    scraper in python

--- webScraping/testScraper.py
+++ webScraping/testScraper.py
@@ -1,5 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
-page_to_scrape = requests.get("http://quotes.toscrape.com")
+page_to_scrape = requests.get("https://edhrec.com/commanders/gyrus-waker-of-corpses")
 
+soup = BeautifulSoup(page_to_scrape.text, "html.parser")
+names = soup.findAll("span", attrs={"class":"Card_name__Mpa7S"})
+
+for name in names:
+    print(name.text)
